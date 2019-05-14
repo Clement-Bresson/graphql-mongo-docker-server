b8bf254c65dc90bc4a081f76c6ec034d78a014d5
Clement-Bresson <clement0bresson@gmail.com>
2019-05-14 12:37:02 +0100

    Docker configuration

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,21 @@ const mongoose = require('mongoose');
 
 const createModels = require('./models');
 
+const dbName =
+  process.env.NODE_ENV === 'development' ? 'database-test' : 'database';
+
+const url = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${
+  process.env.MONGO_INITDB_ROOT_PASSWORD
+}@${dbName}:27017/greeno?authMechanism=SCRAM-SHA-1&authSource=admin`;
+
+const options = {
+  useNewUrlParser: true,
+  reconnectTries: 60,
+  reconnectInterval: 1000
+};
+
 module.exports = function createStore() {
-  mongoose.connect('mongodb://localhost/greeno', { useNewUrlParser: true });
+  mongoose.connect(url, options);
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:')); //eslint-disable-line
---
+++ test/unit.test.js
@@ -0,0 +1,5 @@
+describe('Test', () => {
+  it('is passing', () => {
+    expect(true).toEqual(true);
+  });
+});
